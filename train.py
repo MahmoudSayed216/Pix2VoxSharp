349f72f309f8f8a478df87b00b0efeb40eefe472
Mahmoud Sayed <96210654+MahmoudSayed216@users.noreply.github.com>
2025-03-22 09:49:09 +0200

    Update train.py

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ def compute_validation_metrics(model, loss_fn, loader, THRESHOLDS, ITERATIONS_PE
     IOU_30 = []
     IOU_35 = []
     IOU_40 = []
-
+    IoUs = [[] for _ in range(len(THRESHOLDS))]
     with torch.no_grad():
         for idx, batch in enumerate(loader):
             images, volumes = batch
@@ -36,21 +36,14 @@ def compute_validation_metrics(model, loss_fn, loader, THRESHOLDS, ITERATIONS_PE
 
             loss = loss_fn(outputs, volumes)
             VAL_LOSS_ACCUMULATOR+=loss.item()
-            iou20, iou25, iou30, iou35, iou40 = compute_iou(outputs, volumes, ths=THRESHOLDS)  ## ASSUMING THEY ARE 3
-            IOU_20.append(iou20)
-            IOU_25.append(iou25)
-            IOU_30.append(iou30)
-            IOU_35.append(iou35)
-            IOU_40.append(iou40)
+            _IOUs = compute_iou(outputs, volumes, ths=THRESHOLDS)  ## ASSUMING THEY ARE 3
+            for i, IoU in enumerate(_IOUs):
+                IoUs[i].append(IoU)
 
         mean_val_loss = VAL_LOSS_ACCUMULATOR/ITERATIONS_PER_EPOCH
-        mean_IOU_20 = sum(IOU_20)/len(IOU_20)
-        mean_IOU_25 = sum(IOU_25)/len(IOU_25)
-        mean_IOU_30 = sum(IOU_30)/len(IOU_30)
-        mean_IOU_35 = sum(IOU_35)/len(IOU_35)
-        mean_IOU_40 = sum(IOU_40)/len(IOU_40)
+        mean_IoUs = [sum(ious)/len(ious) for ious in IoUs]
 
-    return mean_val_loss, [mean_IOU_20, mean_IOU_25, mean_IOU_30, mean_IOU_35, mean_IOU_40]
+    return mean_val_loss, mean_IoUs
 
 def gaussian_random(low=1, high=12):
     mu = 6.5
@@ -109,18 +102,28 @@ def train(configs):
 
 
 
-
-    EPOCHS = configs["train"]["epochs"]
-    START_EPOCH = configs["train"]["start_epoch"]
-    THRESHOLDS = configs["thresholds"]
-
     model = full_model.Pix2VoxSharp(configs).to(configs["device"])
     trainable_params = [p for p in model.parameters() if p.requires_grad]
     optimizer = torch.optim.AdamW(params=trainable_params, lr=configs["optim"]["lr"])
+
+    if not configs["train"]["continue_from_checkpoint"]:
+        START_EPOCH = 0
+        current_best_mAP = 0
+    else: 
+        print("loading checkpoint")
+        START_EPOCH, current_best_mAP, model_state_dict, optimizer_state_dict = network_utils.load_checkpoint(configs)
+        model.load_state_dict(model_state_dict)
+        optimizer.load_state_dict(optimizer_state_dict)
+        print("state dict loaded without any problems")
+
+    
+    THRESHOLDS = configs["thresholds"]
+    EPOCHS = configs["train"]["epochs"]
     loss_fn = VoxelLoss(weight=10)
     ITERATIONS_PER_EPOCH_TRAIN = int(len(train_dataset)/BATCH_SIZE)
     ITERATIONS_PER_EPOCH_VAL = int(len(val_dataset)/BATCH_SIZE)
-    current_best_iou = 0
+
+
     for epoch in range(START_EPOCH, EPOCHS):
         LOG("TRAINING")
         LOG("EPOCH", epoch+1)
@@ -139,37 +142,40 @@ def train(configs):
             loss.backward()
             optimizer.step()
             if idx % 50 == 0:
-
                 LOG("current loss", loss.item())
 
         average_epoch_loss = TRAIN_LOSS_ACCUMUlATOR/ITERATIONS_PER_EPOCH_TRAIN
 
         LOG("TESTING")
-        valid_loss, valid_IoU = compute_validation_metrics(model, loss_fn, val_loader, THRESHOLDS, ITERATIONS_PER_EPOCH_VAL, configs)
-        mean_iou = sum(valid_IoU)/len(valid_IoU)
+        valid_loss, valid_IoUs = compute_validation_metrics(model, loss_fn, val_loader, THRESHOLDS, ITERATIONS_PER_EPOCH_VAL, configs)
+        mAP = sum(valid_IoUs)/len(valid_IoUs)
 
         LOG("average train loss", average_epoch_loss)
         LOG("average test loss", valid_loss)
-        LOG("test IoU @ different THs", valid_IoU)
-        LOG("mean test IoU", mean_iou)
+        LOG("test IoU @ different THs", valid_IoUs)
+        for TH, iou in zip(THRESHOLDS, valid_IoUs):
+            LOG(f"iou @ {TH}: {iou}")
+        LOG(f"mAP@{THRESHOLDS[0]}-{THRESHOLDS[-1]}", mAP)
         
 
 
-        if mean_iou > current_best_iou:
-            current_best_iou = mean_iou
+        if mAP > current_best_mAP:
+            current_best_mAP = mAP
             CHECKPOINT(f"IoU has scored a higher value at epoch {epoch+1}. Saving Weights...")
             writer.add_line(f"IoU has scored a higher value at epoch {epoch+1}. Saving Weights...")
             weights_path = os.path.join(configs["train_path"], "weights", "best.pth")
-            network_utils.save_checkpoints(weights_path, epoch+1,model, optimizer, mean_iou, epoch+1)
-            samples_path = os.path.join(configs["train_path"], "samples", f"output{epoch+1}.pth")
-            torch.save(outputs, samples_path)
+            network_utils.save_checkpoints(weights_path, epoch+1,model, optimizer, mAP, epoch+1)
+            volumes_path = os.path.join(configs["train_path"], "samples", f"output{epoch+1}.pth")
+            images_path = os.path.join(configs["train_path"], "samples", f"images{epoch+1}.pth")
 
+            torch.save(outputs, volumes_path)
+            torch.save(images, images_path)
             LOG("tensor saved")
             
         if (epoch+1) % configs["train"]["save_every"] == 0:
             weights_path = os.path.join(configs["train_path"], "weights", "last.pth")
             CHECKPOINT("Saving last Weights...")
-            network_utils.save_checkpoints(weights_path, epoch+1,model, optimizer, mean_iou, epoch+1)
+            network_utils.save_checkpoints(weights_path, epoch+1,model, optimizer, mAP, epoch+1)
 
             if (epoch+1) % configs["train"]["reduce_lr_epoch"]== 0:
                 LOG("REDUCING LR")
@@ -193,12 +199,10 @@ def train(configs):
         
         writer.add_scaler("TRAIN LOSS", epoch+1, average_epoch_loss)
         writer.add_scaler("VALID LOSS", epoch+1, valid_loss)
-        writer.add_scaler("VALID IoU@20", epoch+1, valid_IoU[0])
-        writer.add_scaler("VALID IoU@25", epoch+1, valid_IoU[1])
-        writer.add_scaler("VALID IoU@30", epoch+1, valid_IoU[2])
-        writer.add_scaler("VALID IoU@35", epoch+1, valid_IoU[3])
-        writer.add_scaler("VALID IoU@40", epoch+1, valid_IoU[4])
-        writer.add_scaler("Mean IoU", epoch+1, mean_iou)
+        for TH, iou in zip(THRESHOLDS, valid_IoUs):
+            writer.add_line(f"IoU @ {TH}: {iou}")
+
+        writer.add_scaler("Mean IoU", epoch+1, mAP)
 
 
 
@@ -219,11 +223,9 @@ def main():
     with open("config.yaml", "r") as f:
         configs = yaml.safe_load(f)
     DEBUGGER_SINGLETON.active = configs["use_debugger"]
-    
+
     train_path = initiate_training_environment(configs["output_dir"])
     configs["train_path"] = train_path
-    LOG("configs", configs)
-    
     train(configs=configs)
 
 
